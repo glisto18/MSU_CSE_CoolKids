a5f1380c9a3bd5c7990a546f0390c4612e48a9fa
glisto18 <r2dan2d2@yahoo.com>
2015-02-04 11:38:06 -0500

    DR - Added sqlite packages and references and initial test class for sqlite debugging

---
+++ BoeingSalesApp/DataAccess/Entities/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace BoeingSalesApp.DataAccess.Entities
+{
+    [Table("TestClass")]
+    class TestClass
+    {
+        []
+        public int mTestID { get; set; }
+
+        public string mTest_Name { get; set; }
+
+        public override string ToString()
+        {
+            return mTest_Name + " " + mTestID;
+        }
+    }
+}
