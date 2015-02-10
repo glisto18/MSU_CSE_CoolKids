7e08b65b9712393e6ea26fd7c5a989c3606a6cc2
glisto18 <r2dan2d2@yahoo.com>
2015-02-09 19:04:16 -0500

    added a few pages to the front end, grid view only needs a list of values to display - Mike

--- BoeingSalesApp/launchMeeting.xaml.cs
+++ BoeingSalesApp/launchMeeting.xaml.cs
@@ -14,29 +14,17 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-// hello world
 
 namespace BoeingSalesApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class launchMeeting : Page
     {
-        public MainPage()
+        public launchMeeting()
         {
             this.InitializeComponent();
-        }
-
-
-        private void onMeetingsRect_Tapped(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(MeetingsView));
-        }
-
-        private void onArt_Tap(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(ArtifactsCatView));
         }
     }
 }
