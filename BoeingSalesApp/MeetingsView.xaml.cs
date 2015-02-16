26a88bd1680739e1efd17cef378995bc21b5a3bd
glisto18 <r2dan2d2@yahoo.com>
2015-02-15 21:24:39 -0500

    Entered commented functions to MeetingsView.xaml.cs for tthe outlook stuff

--- BoeingSalesApp/MeetingsView.xaml.cs
+++ BoeingSalesApp/MeetingsView.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+//using Microsoft.Office.Interop.Outlook
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -35,5 +36,33 @@ namespace BoeingSalesApp
         {
             this.Frame.Navigate(typeof(launchMeeting));
         }
+        /* 
+         * If returned false load page or dialogue  box to say outlook is not installed
+        public static bool IsOutlookInstalled()
+        {
+            try
+            {
+                Type type = Type.GetTypeFromCLSID(new Guid("0006F03A-0000-0000-C000-000000000046")); //Outlook.Application
+                if (type == null) return false;
+                object obj = Activator.CreateInstance(type);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                return true;
+            }
+            catch (COMException)
+            {
+                return false;
+            }
+        }
+         * function to be called by "import meetings" button
+         * private void onImport()
+         * {
+         *      if(IsOutlookInstalled()) this.Frame.Navigate(typeof(SuccessPage));
+         *      else this.Frame.Navigate(typeof(FailPage));
+         *      Microsoft.Office.Interop.Outlook.Application oApp = new Microsoft.Office.Interop.Outlook.Application();
+         *      Microsoft.Office.Interop.Outlook.NameSpace mapiNamespace= oApp.GetNamespace("MAPI");
+         *      Microsoft.Office.Interop.Outlook.MAPIFolder CalendarFolder= mapiNamespace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderCalendar);
+         *      Microsoft.Office.Interop.Outlook.Items outlookCalendarItems = CalendarFolder.Items;
+         * }
+         * */
     }
 }
