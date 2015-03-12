9803896c3da43ad057c7304e9ca7ecae57e6cc43
glisto18 <r2dan2d2@yahoo.com>
2015-03-11 23:52:59 -0700

    DR - Made some additions to the backend of the SB creation page.

--- BoeingSalesApp/BagCreationView.xaml.cs
+++ BoeingSalesApp/BagCreationView.xaml.cs
@@ -96,14 +96,19 @@ namespace BoeingSalesApp
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
             navigationHelper.OnNavigatedTo(e);
+
+            //DR - Below, capture all categories and bind to grid
             CategoryRepository catRepo = new CategoryRepository();
-
             List<BoeingSalesApp.DataAccess.Entities.Category> catList = await catRepo.GetAllAsync();
-
             foreach(BoeingSalesApp.DataAccess.Entities.Category i in catList)
             {
                 this.sourceGrid.Items.Add(i);
             }
+
+            //DR - This could probably be set in the xaml for the grid itself
+            this.sourceGrid.SelectionMode = ListViewSelectionMode.Multiple;
+
+            //DR - Do we capture artifacts without categories?  Do those exist?
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
@@ -116,6 +121,26 @@ namespace BoeingSalesApp
         private void sourceGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //DR - The new salesbag to be added to the db
+            DataAccess.Entities.SalesBag newbag = new DataAccess.Entities.SalesBag();
+
+            //DR - This is the means of querying the database with an interface
+            //  Found the code here https://msdn.microsoft.com/en-us/library/bb341406(v=vs.110).aspx
+            IEnumerable<DataAccess.Entities.Category> query =
+                this.sourceGrid.SelectedItems.Cast<DataAccess.Entities.Category>().Select(x => x);
+
+            foreach(DataAccess.Entities.Category i in query)
+            {
+                //DR - We need a means of associating salesbags and categories.
+//               DataAccess.Entities.SalesBag_Category newSalesBagCat = new DataAccess.Entities.SalesBag_Category();
+            }
+
+            //After bag is created and saved to db, navigate back to the salesbag page.
+            this.Frame.Navigate(typeof(SalesBagsView));
         }
     }
 }
