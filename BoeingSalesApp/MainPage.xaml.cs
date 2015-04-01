ccb983492231ea04fae6cd853cfbd65b98fe3305
westjour <westjour@msu.edu>
2015-04-01 09:58:28 -0400

    added table containing meeting data in pdf

--- BoeingSalesApp/MainPage.xaml.cs
+++ BoeingSalesApp/MainPage.xaml.cs
@@ -79,7 +79,6 @@ namespace BoeingSalesApp
             //C:\Users\Team Boeing\Downloads\BoeingArtifactFolder
             Windows.Storage.StorageFolder folder = await fileStore.GetArtifactFolder();
 
-            // Create empty PDF file.
             Windows.Storage.StorageFile file = null;
             try
             {
@@ -89,14 +88,14 @@ namespace BoeingSalesApp
             {
             }
 
-            if (file != null)
+            /*if (file != null)
             {
                 await Windows.Storage.FileIO.WriteTextAsync(file, string.Empty);
-            }
+            }*/
 
             // Open to PDF file for read/write.
-            Windows.Storage.StorageFile sampleFile = await folder.GetFileAsync(path);
-            var stream = await sampleFile.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+            Windows.Storage.StorageFile samplefile = await folder.GetFileAsync(path);
+            var stream = await samplefile.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
 
             // Create an instance of the document class which represents the PDF document itself.
             Document document = new Document(PageSize.A4, 25, 25, 30, 30);
@@ -121,57 +120,64 @@ namespace BoeingSalesApp
             logo.ScalePercent(5f);
 
             // Create document header
-            Chunk headerChunk = new Chunk("Exit Survey Summary",
-                FontFactory.GetFont(FontFactory.TIMES_ROMAN, 26, Font.NORMAL, new BaseColor(0,0,0)));
+            Font headerFont = FontFactory.GetFont(FontFactory.TIMES_ROMAN, 26, Font.NORMAL, new BaseColor(0, 0, 0));
+            Chunk headerChunk = new Chunk("Exit Survey Summary", headerFont);
             headerChunk.SetUnderline(0.5f, -1.5f);
             Paragraph header = new Paragraph();
             header.Add(headerChunk);
             header.Alignment = iTextSharp.text.Element.ALIGN_CENTER;
             header.SpacingBefore = 15.0f;
 
-            // Meeting duration
-            Chunk location = new Chunk("Location: Detroit, MI\n",
-                FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, new BaseColor(0, 0, 0)));
-            Chunk beginTime = new Chunk("Start Time: 5/5/15 10:03 AM CT\n",
-                FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, new BaseColor(0, 0, 0)));
-            Chunk endTime = new Chunk("End Time:  5/5/15 11:00 AM CT\n",
-                FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, new BaseColor(0, 0, 0)));
-            Chunk duration = new Chunk("Duration: 57 mins\n",
-                FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, new BaseColor(0, 0, 0)));
-            Chunk commentChunk = new Chunk("\nComments:\n",
-                FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, new BaseColor(0, 0, 0)));
-            Chunk contact = new Chunk("Primary Contact: John Doe\n",
-                FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, new BaseColor(0, 0, 0)));
-            
-            string text = @"Customer would like further information regarding Advanced Acoustics Services 
-for anti-submiarine warfare.";
-            Chunk comment = new Chunk(text,
-                FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, new BaseColor(0, 0, 0)));
-            Paragraph commentPara = new Paragraph(comment);
-            commentPara.SpacingBefore = 5.0f;
+            Font font = FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, new BaseColor(0, 0, 0));
+            // Table vertical headers
+            Phrase header_loc = new Phrase("Location", font);
+            Phrase loc = new Phrase("Detroit, MI", font);
+            Phrase header_beginTime = new Phrase("Start Time", font);
+            Phrase beginTime = new Phrase("5/5/15 10:03 AM CT", font);
+            Phrase header_endTime = new Phrase("End Time", font);
+            Phrase endTime = new Phrase("5/5/15 11:00 AM CT", font);
+            Phrase header_duration = new Phrase("Duration", font);
+            Phrase duration = new Phrase("57 mins", font);
+            Phrase header_contact = new Phrase("Primary Contact", font);
+            Phrase contact = new Phrase("John Doe", font);
 
-            Chunk rating = new Chunk("Event Rating --\n",
-                FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, new BaseColor(0, 0, 0)));
-            Chunk assess = new Chunk("Overall Assessment --\n",
-                FontFactory.GetFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, new BaseColor(0, 0, 0)));
-            
-            Paragraph meetingData = new Paragraph();
-            meetingData.SpacingBefore = 15.0f;
-            meetingData.Add(location);
-            meetingData.Add(beginTime);
-            meetingData.Add(endTime);
-            meetingData.Add(duration);
-            meetingData.Add(contact);
-            meetingData.Add(rating);
-            meetingData.Add(assess);
+            Phrase header_rating = new Phrase("Event Rating", font);
+            Phrase rating = new Phrase("--", font);
+            Phrase header_assess = new Phrase("Overall Assessment", font);
+            Phrase assess = new Phrase("--", font);
 
-            meetingData.Add(commentChunk);
-            meetingData.Add(commentPara);
+            Phrase header_comment = new Phrase("Comments", font);
+            string text = "Customer would like further information regarding Advanced Acoustics Services for anti-submiarine warfare.";
+            Phrase comment = new Phrase(text, font);
+
+            PdfPTable table = new PdfPTable(2);
+            table.DefaultCell.Border = Rectangle.NO_BORDER;
+            table.SpacingBefore = 20.0f;
+            int[] columnWidths = new int[] { 20, 60 };
+            table.SetWidths(columnWidths);
+
+            // Cells are added to table from left to right and top to bottom
+            table.AddCell(header_loc);
+            table.AddCell(loc);
+            table.AddCell(header_beginTime);
+            table.AddCell(beginTime);
+            table.AddCell(header_endTime);
+            table.AddCell(endTime);
+            table.AddCell(header_duration);
+            table.AddCell(duration);
+            table.AddCell(header_contact);
+            table.AddCell(contact);
+            table.AddCell(header_rating);
+            table.AddCell(rating);
+            table.AddCell(header_assess);
+            table.AddCell(assess);
+            table.AddCell(header_comment);
+            table.AddCell(comment);
 
             // Add element to document
             document.Add(logo);
             document.Add(header);
-            document.Add(meetingData);
+            document.Add(table);
 
             // Close the document
             document.Close();
